fb06ead9665e8da5bb7881d2bac27451c3302a69
ghc888 <164523225@qq.com>
2017-07-31 21:06:44 +0800

    client object

---
+++ src/test/TestIni.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+"config"
+	"fmt"
+)
+
+
+func main() {
+
+	config.WriteKey("/Users/ghc/IdeaProjects/dbMag/src/etc/my.cnf","client","max_packet_size","32M")
+
+	size:=config.ReadSectionKey("/users/ghc/IdeaProjects/dbMag/src/etc/my.cnf","client","max_packet_size")
+	fmt.Println(size)
+	//config.WriteSection("/tmp/my.cnf","client")
+}
