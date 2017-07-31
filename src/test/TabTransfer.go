29091075fef151ff21711a16c08da6052f598682
ghc888 <164523225@qq.com>
2017-07-31 21:13:37 +0800

    client object

--- src/test/TabTransfer.go
+++ src/test/TabTransfer.go
@@ -1,16 +1,28 @@
 package main
 
+import "fmt"
+
 type TableInfo struct {
-	dbconn DbConn   //为所在的数据库 connection
-	name string     //表的名字
-	maxid uint64   //表中最大的id值 表必须有一个id属性而且为主键
+	dbconn DbConn //为所在的数据库 connection
+	name   string //表的名字
+	maxid  uint64 //表中最大的id值 表必须有一个id属性而且为主键
 }
 
 type DbConn struct {
-	host string
-	user string
+	host   string
+	user   string
 	passwd string
-	port uint32
+	port   uint32
+}
+
+type TableInterFace interface {
+	GetData()
+	SendData()
 }
 
 
+
+
+func main() {
+	fmt.Println("hello world")
+}
