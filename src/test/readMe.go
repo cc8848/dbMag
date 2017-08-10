30d54a63707c07944ca2e2ba736645ab86192365
ghc888 <164523225@qq.com>
2017-08-10 21:28:22 +0800

    client object

---
+++ src/test/readMe.go
@@ -0,0 +1,5 @@
+package main
+
+//https://mariadb.com/resources/blog/how-stream-change-data-through-mariadb-maxscale-using-cdc-api
+
+//https://mariadb.com/resources/blog/data-streaming-mariadb-maxscale
